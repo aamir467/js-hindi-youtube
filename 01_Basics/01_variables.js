9e6c6744f376ab1d77e59a4e26519500cc99b320
Aamir Shamim <aamirshamimabc@gmail.com>
2025-02-13 11:43:04 +0000

    variables js done

---
+++ 01_Basics/01_variables.js
@@ -0,0 +1,13 @@
+const accountID = 12345
+let accountEmail = "aamirshamimabc@gmail.com"
+var accountPassword = "12345"
+accountCity = "Dhanbad"
+let accountState;
+
+
+// accountId = 2 // not allowed 
+
+accountEmail = "aamirshamim@gmail.com"
+accountCity = "Jamshedpur"
+console.table([accountID, accountEmail, accountPassword, accountCity, accountState])
+

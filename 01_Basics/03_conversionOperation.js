4005a94574abde9afef1b6b7d1121a54d5ed1cc0
Aamir Shamim <aamirshamimabc@gmail.com>
2025-02-13 17:32:04 +0000

    conversion of datatypes

---
+++ 01_Basics/03_conversionOperation.js
@@ -0,0 +1,23 @@
+let score = true
+
+console.log(typeof score);
+
+console.log(score)
+
+let valueInNumber = Number(score)
+console.log(typeof valueInNumber);
+console.log(valueInNumber)
+
+// "33" =>33 
+
+let isloggedIn = 0
+let booleanISLoggedIn = Boolean(isloggedIn)
+console.log(booleanISLoggedIn)
+
+// 1 => true ; 0=> false ; " " => false ; "Aamir" => true 
+
+let someNumber = 33
+
+let stringNumber = String(someNumber)
+console.log(stringNumber)
+console.log(typeof stringNumber)

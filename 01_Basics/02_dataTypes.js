4b1e3aaf13c1fbe266e473315365e5445080d014
Aamir Shamim <aamirshamimabc@gmail.com>
2025-02-13 12:42:28 +0000

    dataTypes done

---
+++ 01_Basics/02_dataTypes.js
@@ -0,0 +1,21 @@
+let name = "Aamir" // string
+let age = 21    // number
+let isloggedIn = false  // boolean
+
+// number => 2 to power 53
+// bigint => large website like fb , reddit , stockmarket 
+// string => " "
+// boolean => true/false
+// null => standalone value 
+// undefine =>
+// symbol => unique
+
+
+// object 
+
+console.log(typeof undefined); // undefined
+console.log(typeof null); // object
+
+console.log(typeof "Aamir")
+
+
